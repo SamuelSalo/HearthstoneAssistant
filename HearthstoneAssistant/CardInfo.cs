a719730ed0858a7fec3784507434b30684e20d09
Samuel Salo <samuel.j.salo@gmail.com>
2019-12-21 03:27:18 +0200

    Add project files.

---
+++ HearthstoneAssistant/CardInfo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HearthstoneAssistant
+{
+
+    public class CardInfo
+    {
+        public string cardId { get; set; }
+        public string name { get; set; }
+        public string cardSet { get; set; }
+        public string type { get; set; }
+        public string faction { get; set; }
+        public string rarity { get; set; }
+        public int cost { get; set; }
+        public int attack { get; set; }
+        public int health { get; set; }
+        public string text { get; set; }
+        public string flavor { get; set; }
+        public string artist { get; set; }
+        public bool collectible { get; set; }
+        public bool elite { get; set; }
+        public string race { get; set; }
+        public string img { get; set; }
+        public string imgGold { get; set; }
+        public string locale { get; set; }
+    }
+}
---
+++ HearthstoneAssistant/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System.Windows;
+using System.Net;
+using System.IO;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace HearthstoneAssistant
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        protected string json = string.Empty;
+        protected const string apiurl = "https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/";
+        protected const string apikey = "831129d93cmsh608e397103ead70p1dd02bjsnf164767338db";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        protected private string Join (string[] strings)
+        {
+            string joined = string.Empty;
+
+            foreach (string str in strings)
+            {
+                joined += str;
+            }
+
+            return joined;
+        }
+
+        private void Search(object sender, RoutedEventArgs e)
+        {
+            string cardname = System.Uri.EscapeUriString(SearchBox.Text);
+            
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(Join(new string[] {apiurl, cardname} ));
+
+            request.Method = WebRequestMethods.Http.Get;
+
+            request.Headers["x-rapidapi-host"] = "omgvamp-hearthstone-v1.p.rapidapi.com";
+            request.Headers["x-rapidapi-key"] = apikey;
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                json = reader.ReadToEnd();
+            }
+            MessageBox.Show(json);
+        }
+    }
+}
